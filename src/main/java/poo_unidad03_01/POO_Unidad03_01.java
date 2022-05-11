c28e7fcd4bdce9e425a162636a4eb3914fd269a4
GEOVANNY <GEOVANNY@172.16.221.111>
2022-05-11 08:11:54 -0500

    Ejemplo 1

---
+++ src/main/java/poo_unidad03_01/POO_Unidad03_01.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo_unidad03_01;
+
+public class POO_Unidad03_01 {
+    
+/**
+ *
+ * @author GEOVANNY
+ * @param args the command kine arguments
+ */
+    
+public static void main(String [] args){
+    
+        //var v = new Ventana030101("Ventana 2022-05-10",800,600);
+       // v.setVisible(true);
+        var v = new Ventana030102("Ventana 2022-05-10");
+        v.setVisible(true);
+        
+    }
+    
+}
---
+++ src/main/java/poo_unidad03_01/Ventana030102.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo_unidad03_01;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author GEOVANNY
+ */
+public class Ventana030102 extends JFrame{
+
+    private JPanel jPanel1;
+    private List<JLabel> jLabelList;
+    
+   public Ventana030102(String title) {
+        super(title);
+        this.setSize(800, 600);
+        this.setLocation(50, 100);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.iniciarComponentes();
+        this.setContentPane(this.jPanel1);
+    }
+
+   public void iniciarComponentes(){
+        this.iniciarPanel();
+        this.iniciarEtiquetas();
+   }
+   
+   public void iniciarPanel(){
+       this.jPanel1 = new JPanel();
+       this.jPanel1.setBackground(Color.YELLOW);
+       this.jPanel1.setBorder(BorderFactory.createTitledBorder("Panel Principal"));
+       this.setLayout(new FlowLayout());
+   }
+   
+   public void iniciarEtiquetas(){
+       this.jLabelList = new ArrayList<>();
+       this.jLabelList.add(new JLabel());
+       this.jLabelList.add(new JLabel());
+       this.jLabelList.add(new JLabel());
+       this.jLabelList.get(0).setBackground(Color.ORANGE);
+       this.jLabelList.get(0).setOpaque(true);
+       this.jLabelList.get(0).setText("Electrónica y automatización");
+       this.jPanel1.add(this.jLabelList.get(0));
+   }
+
+   
+}
